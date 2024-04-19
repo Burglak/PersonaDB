0248864d4290fd03f82ee87dbd172087565f71b2
Burglak <you@example.com>
2024-04-20 00:23:20 +0200

    adding login panel

--- src/main/java/com/example/personadb/App.java
+++ src/main/java/com/example/personadb/App.java
@@ -2,7 +2,13 @@ package com.example.personadb;
 
 import com.example.personadb.model.*;
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -11,34 +17,52 @@ import org.hibernate.cfg.Configuration;
 import java.util.List;
 
 public class App extends Application {
+    private double xOffset = 0;
+    private double yOffset = 0;
     @Override
     public void start(Stage stage) throws Exception {
-        Person person = new Person("john123", "password123", "John Doe", "john@example.com", "standard", 500, 0, 100, 100);
+        //Person person = new Person("john123", "password123", "John Doe", "john@example.com", "standard", 500, 0, 100, 100);
         //Persona persona = new Persona("Izanagi", "chariot", 3, 4, 5, 6, 7, 8);
         //PersonPersona personPersona = new PersonPersona(1, 2, 1);
-        PersonaAffinity personaAffinity = new PersonaAffinity(2, 2, "weakness");
+        //PersonaAffinity personaAffinity = new PersonaAffinity(2, 2, "weakness");
 
-        Configuration cfg = new Configuration().configure();
+//        Configuration cfg = new Configuration().configure();
+//
+//        // add annotated classes
+//        cfg.addAnnotatedClass(Person.class);
+//        cfg.addAnnotatedClass(Persona.class);
+//        cfg.addAnnotatedClass(Affinity.class);
+//        cfg.addAnnotatedClass(Skill.class);
+//        cfg.addAnnotatedClass(PersonPersona.class);
+//        cfg.addAnnotatedClass(PersonPersonaSkill.class);
+//        cfg.addAnnotatedClass(PersonaAffinity.class);
 
-        // add annotated classes
-        cfg.addAnnotatedClass(Person.class);
-        cfg.addAnnotatedClass(Persona.class);
-        cfg.addAnnotatedClass(Affinity.class);
-        cfg.addAnnotatedClass(Skill.class);
-        cfg.addAnnotatedClass(PersonPersona.class);
-        cfg.addAnnotatedClass(PersonPersonaSkill.class);
-        cfg.addAnnotatedClass(PersonaAffinity.class);
+//        SessionFactory sf = cfg.configure().buildSessionFactory();
+//        Session session = sf.openSession();
+//        Transaction tx = session.beginTransaction();
+//        tx.commit();
+        stage.initStyle(StageStyle.UNDECORATED); //remove title bar
+        Parent root = FXMLLoader.load(getClass().getResource("login_page.fxml"));
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                xOffset = mouseEvent.getSceneX();
+                yOffset = mouseEvent.getSceneY();
+            }
+        });
 
-        SessionFactory sf = cfg.configure().buildSessionFactory();
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                stage.setX(mouseEvent.getScreenX() - xOffset);
+                stage.setY(mouseEvent.getScreenY() - yOffset);
+            }
+        });
 
-        Session session = sf.openSession();
+        Scene scene = new Scene(root);
 
-        Transaction tx = session.beginTransaction();
-
-        session.save(person);
-
-        tx.commit();
-
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,7 @@ module com.example.personadb {
     requires java.naming;
 
 
+
     opens com.example.personadb to javafx.fxml, org.hibernate.orm.core;
     opens com.example.personadb.model to org.hibernate.orm.core;
     exports com.example.personadb;
